1d541a0db8d1569265b07690b266ec6a7d9ff70b
HiTechPsyCoder <bhavyalogicom@gmail.com>
2022-06-23 14:49:43 +0530

    init

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,9 @@
 import papermill as pm
 def lambda_handler(event, context):   
+    pm.execute_notebook(
+   'notebook.ipynb',
+   'notebook_output.ipynb',
+)
     response = {'text':'lamda is working'}
     print(response['text'])
     return response['text']
